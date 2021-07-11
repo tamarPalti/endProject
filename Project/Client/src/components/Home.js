fe5ebfe9fd2b8c9f81374f2f7ff3b4a81d95920c
hilagamliel <82827452+hilagamliel@users.noreply.github.com>
2021-07-11 19:34:18 +0300

    changes detailes to model and start res

--- Project/Client/src/components/Home.js
+++ Project/Client/src/components/Home.js
@@ -4,9 +4,7 @@ import { Link, Route } from 'react-router-dom';
 import UsersSearch from './search/UseresSearch';
 import BusinessSearch from './search/BusinessSearch';
 import Search from './search/Search';
-import UserDetailes from './Detailes/UserDetailes';
 import { connect } from "react-redux";
-import BusinessDetailes from './Detailes/BusinessDetailes';
 const Home = (props) => {
     console.log("kkkk");
     
@@ -14,9 +12,6 @@ const Home = (props) => {
         <div>
     <Route path={ '/search'}>  <Search /> </Route>
     
-    {/*  לוקח נתונים מהסטייט הכללי UserDetailes*/}
-    <Route path={ '/userDetailes/:id'}>  <UserDetailes /> </Route>
-    <Route path={ '/businessDetailes/:id'}>  <BusinessDetailes /> </Route>
     </div> );
 }
 const mapStateToProps = (state) => {
