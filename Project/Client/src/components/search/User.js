4feb4e4f61565e9a22399b2d04f3602fbee66dfd
hilagamliel <82827452+hilagamliel@users.noreply.github.com>
2021-07-11 12:43:41 +0300

    change leter in search in hebrew and english

--- Project/Client/src/components/search/User.js
+++ Project/Client/src/components/search/User.js
@@ -21,15 +21,24 @@ const User = (props) => {
     }, []);
     return (<div><Link to={'/userDetailes/' + props.user._id} style={{ textDecoration: 'none', color: 'black' }}> <div className="place_user" onClick={() => props.SelectedUser(props.user)}>
         <p className="display">{checkName(props.user.lastName) ? Laststart : Lastend}</p>
-        {!checkName(props.user.firstName)&&(Lastend[0] == ' '||props.ColorLastName[props.ColorLastName.length-1]==' ' )? <p className="display">&nbsp;</p> : null}
+        {!checkName(props.user.lastName) && (Lastend[0] == ' ' || props.ColorLastName[props.ColorLastName.length - 1] == ' ') ? <p className="display">&nbsp;</p> : null}
+        {checkName(props.user.firstName) && (Laststart[Laststart.length - 1] == ' ' || props.ColorLastName[props.ColorLastName.length - 1] == ' ') ? <p className="display">&nbsp;</p> : null}
+
         <p className="color_name display">{props.ColorLastName}</p>
-        {checkName(props.user.firstName)&&(Lastend[0] == ' '||props.ColorLastName[props.ColorLastName.length-1]==' ' ) ? <p className="display">&nbsp;</p> : null}
+        {!checkName(props.user.lastName) && (Laststart[Laststart.length-1] == ' ' || props.ColorLastName[props.ColorLastName.length - 1] == ' ') ? <p className="display">&nbsp;</p> : null}
+        {checkName(props.user.lastName) && (Lastend[0] == ' ' || props.ColorLastName[props.ColorLastName.length - 1] == ' ') ? <p className="display">&nbsp;</p> : null}
+
         <p className="display">{checkName(props.user.lastName) ? Lastend : Laststart}</p>
         <p className="display">&nbsp;</p>
         <p className="display">{checkName(props.user.firstName) ? start : end}</p>
-        {!checkName(props.user.lastName)&&(end[0] == ' ' ||props.ColorFirstName[props.ColorFirstName.length-1]==' ')? <p className="display">&nbsp;</p> : null}
+      
+        {checkName(props.user.lastName) && (start[start.length - 1]) == ' ' ? <p className="display">&nbsp;</p> : null}
+        {!checkName(props.user.lastName) && (end[0] == ' ' || props.ColorFirstName[props.ColorFirstName.length - 1] == ' ') ? <p className="display">&nbsp;</p> : null}
+       
         <p className="color_name display">{props.ColorFirstName}</p>
-        {checkName(props.user.lastName)&&(end[0] == ' '||props.ColorFirstName[props.ColorFirstName.length-1]==' ' )? <p className="display">&nbsp;</p> : null}
+        {checkName(props.user.lastName) && (end[0] == ' ' || props.ColorFirstName[props.ColorFirstName.length - 1] == ' ') ? <p className="display">&nbsp;</p> : null}
+        {!checkName(props.user.lastName) && (start[start.length - 1]) == ' ' ? <p className="display">&nbsp;</p> : null}
+        
         <p className="display">{checkName(props.user.firstName) ? end : start}</p>
         <img className="img_ico" src={ico}></img>
     </div>
