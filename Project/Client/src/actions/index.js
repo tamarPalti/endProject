ccf0d427d54edbe000141d2e9900bd0ebdcffe1f
hilagamliel <82827452+hilagamliel@users.noreply.github.com>
2021-07-15 18:26:38 +0300

    change login in commponnetnt

--- Project/Client/src/actions/index.js
+++ Project/Client/src/actions/index.js
@@ -1,8 +1,4 @@
 export { GetAllUsers, SaveResultUsers, SearchUsers, SelectedUser, NoSelectedUser, DeleteResultUser,
-<<<<<<< HEAD
-     ChangeColorFirstName, ChangeColorLastName} from './users';
-=======
-     ChangeColorFirstName, ChangeColorLastName,SignIn,GetCurrentUser} from './users';
->>>>>>> 4424f6097e88c845d7a6ae6f650e27c7419cd04d
+     ChangeColorFirstName, ChangeColorLastName,SignIn} from './users';
 export { GetAllBusiness, SaveResultBusiness, SearchBusiness, SelectedBusiness, NoSelectedBusiness, DeleteResultBusiness,
           ChangeColorName} from './business';
