2bda438f4952b9c1db51f4332fcaf07cdec07851
hilagamliel <82827452+hilagamliel@users.noreply.github.com>
2021-08-08 12:54:42 +0300

    sign up

--- Project/Client/src/components/LogIn/SingUp.js
+++ Project/Client/src/components/LogIn/SingUp.js
@@ -73,6 +73,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { connect } from "react-redux";
 
 function Copyright() {
   return (
@@ -109,7 +110,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignUp() {
+function SignUp() {
   const classes = useStyles();
 
   return (
@@ -210,3 +211,8 @@ export default function SignUp() {
     </Container>
   );
 }
+const mapStateToProps = (state) => {
+
+    return {};
+}
+export default connect(mapStateToProps, {})(SignUp);
