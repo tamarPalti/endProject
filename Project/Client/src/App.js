f7157e301a60c6304dfb3f981fa7d432318e810f
tamarPalti <82827768+tamarPalti@users.noreply.github.com>
2021-10-12 20:55:53 +0300

    התחלנו עיצוב

--- Project/Client/src/App.js
+++ Project/Client/src/App.js
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'semantic-ui-css/semantic.min.css';
-import Home from './components/Home'
+import Home from './components/Home';
+import Heder from './components/Heder';
 import SimpleMap from './components/search/MyLocation'
 import { connect } from "react-redux";
 import MyLocation from './components/search/MyLocation';
 const app = (props) => {
     return (<>
-        <Home/>
+     <Heder Home={Home}/>
+        {/* <Home/> */}
         {/* <SimpleMap/> */}
         {/* <MyLocation /> */}
         {/* console.log("jjj"); */}
